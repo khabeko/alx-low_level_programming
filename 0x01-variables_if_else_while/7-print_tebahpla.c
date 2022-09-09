7fa0fc50b1b624aafaad1301c72885293ac29f56
khabeko <khabeko@outlook.com>
2022-09-09 12:18:31 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
