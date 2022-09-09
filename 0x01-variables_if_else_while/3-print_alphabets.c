6bd0cdb9cf9ca82f457ce101b737b4e686f6f18b
khabeko <khabeko@outlook.com>
2022-09-09 11:36:23 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,17 @@
  * Return: Always 0.
  */
 int main(void)
+
 {
 	char letter;
-
+	
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-
-	for (letter ='A'; letter <= 'Z'; letter++)
+	
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
-
+	
 	putchar('\n');
-
-	return(0);
+	
+	return (0);
 }
