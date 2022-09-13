a1a7843094332d73648d47c7968bdd1064ff7da1
khabeko <khabeko@outlook.com>
2022-09-12 22:59:39 -0700

    1. I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - write the lowercase alphabet on a line
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
