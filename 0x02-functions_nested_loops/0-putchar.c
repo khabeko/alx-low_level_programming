954e5ab1aa5e367eb5503c610a01ca355450c00b
khabeko <khabeko@outlook.com>
2022-09-12 22:25:02 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Program that prints _putchar followd by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char _putchar[] = "_putchar";
+	int i = 0;
+
+	while (_putcahr[i] != '\0')
+	{
+		_puthar(_putchar[!]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
