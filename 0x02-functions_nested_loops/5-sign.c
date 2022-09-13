2e7eefdfb3b0f6002c1deb4e33228c41ab6a2bdf
khabeko <khabeko@outlook.com>
2022-09-13 01:01:30 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,23 @@
  */
 int print_sign(int n)
 {
+	int test;
+
 	if (n > 0)
 	{
+		test = 1;
 		_putchar('+');
-		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar('-');
-		return (-1);
+		test = 0;
+		_putchar('0');
 	}
 	else
 	{
-		_putchar('0');
-		return (0);
+		test = -1;
+		_putchar('-');
 	}
-}
+
+	return (test);
+}	
