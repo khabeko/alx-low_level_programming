9a37968a000f005802e0938b198bbf6303543ce6
khabeko <khabeko@outlook.com>
2022-09-14 08:21:11 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two intergers together
+ * @a: value to be used for the sum
+ * @b: second value to be used for the sum.
+ *
+ * Return: int (Sum)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
