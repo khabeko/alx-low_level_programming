8da78bf7ccdfa50a5f7581a3a420df366100d9a7
khabeko <khabeko@outlook.com>
2022-09-19 11:34:48 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - takes the pointer and updates the value
+ * reset_to_98 - function that takes apointer to an int as parameter and
+ * updates the value it points to to 98
+ * @n: This is the entry
  *
- * @n: int to check
- *
- * Return: 0 is success
  */
 void reset_to_98(int *n)
 {
