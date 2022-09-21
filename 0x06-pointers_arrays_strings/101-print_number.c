c770a21d7d73b26e96b95e05cbd5639b1062147f
khabeko <khabeko@outlook.com>
2022-09-21 11:51:40 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
-* print_number - prints an integer
-*@n:integer to be printed
-*
-*/
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+
 void print_number(int n)
 {
 	unsigned int num = n;
