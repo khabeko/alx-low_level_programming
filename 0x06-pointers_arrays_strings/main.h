c34bdd5ec36d35856f2e517b2643769211a2c7b1
khabeko <khabeko@outlook.com>
2022-09-21 06:08:07 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
+char *_strcat(char *dest, char *src);
+
+#endif /*MAIN_H*/
