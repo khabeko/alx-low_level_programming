9688eee8a8e2043cda2a44a7219d98d4647aee1a
khabeko <khabeko@outlook.com>
2022-09-27 01:56:24 -0700

    corrections

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,7 +11,7 @@ void print_chessboard(char (*a)[8])
 
 	for (indx1 = 0; a[indx1][7]; indx1++)
 	{
-		for (indx2 = 0; a[indx2][7]; indx2++)
+		for (indx2 = 0; indx2 < 8; indx2++)
 			_putchar(a[indx1][indx2]);
 
 		_putchar('\n');
