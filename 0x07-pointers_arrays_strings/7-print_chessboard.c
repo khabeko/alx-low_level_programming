d19cea2370f0dbf35e4ff01bc9e48810e286fcbf
khabeko <khabeko@outlook.com>
2022-09-27 02:54:55 -0700

    corrections

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * print_chessboard - description
- * @a: board
- * Return: void
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
@@ -16,7 +14,6 @@ void print_chessboard(char (*a)[8])
 		{
 			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
