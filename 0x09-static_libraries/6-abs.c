5686abca962f1d26adf434253b97d02e544d2ccc
khabeko <khabeko@outlook.com>
2022-09-30 11:45:40 -0700

    task 0

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs -return the absolute value of a given integer.
+ * @i: Value use to find the absolute value.
+ *
+ *
+ * Return: the absolute value.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		return (-i);
+	return (i);
+}
